38fc5cbd1c1e6d933e7b2ffe6f91aaea13764fd5
Kanailal Manna <kanailalmanna2003@gmail.com>
2023-10-23 23:07:14 +0530

    🌃 Mood addition

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import { UserButton } from "@clerk/nextjs"
 import { MobileSidebar } from "@/components/mobile-sidebar"
-
+import { ModeToggle } from "@/components/theme-toggle"
 
 export const Navbar = () => {
     return (
         <div className="flex items-center p-4">
             <MobileSidebar />
-            <div className="flex w-full justify-end">
+            <div className="flex w-full justify-end items-center gap-4 ">
+                <ModeToggle/>
                 <UserButton afterSignOutUrl="/" />
             </div>
         </div>
