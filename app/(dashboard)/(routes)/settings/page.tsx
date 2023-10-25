b1b3f404b66edfcb85062eb07d70965eb117b0e4
Kanailal Manna <kanailalmanna2003@gmail.com>
2023-10-25 22:38:31 +0530

    ⚙️ page generated

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { Empty } from "@/components/empty"
+
+const Settings = () => {
+    return (
+        <div className="flex items-center justify-center">
+            <h1>Work in Progress!</h1>
+            <Empty label="Setting"/>
+        </div>
+    )
+}
