caa82fc2644205d9f3ce8dfd48227067b0a91e13
Kanailal Manna <kanailalmanna2003@gmail.com>
2023-10-25 22:58:38 +0530

    ⚙️ page intregation

--- app/(dashboard)/(routes)/settings/page.tsx
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -4,9 +4,8 @@ import { Empty } from "@/components/empty"
 
 const Settings = () => {
     return (
-        <div className="flex items-center justify-center">
-            <h1>Work in Progress!</h1>
-            <Empty label="Setting"/>
+        <div >
+            <Empty label="Work In Progress! Setting Page Not implemented yet!"/>
         </div>
     )
 }
