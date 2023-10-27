d20ec53f2cd8fce5adfc13806493d766df8d458b
Kanailal Manna <kanailalmanna2003@gmail.com>
2023-10-27 21:21:22 +0530

    landing page bg theme

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -4,7 +4,7 @@ const LandingLayout = ({
     children: React.ReactNode;
 }) => {
     return (
-        <main className="h-full bg-gradient-to-r from-blue-950 to-cyan-950 overflow-auto">
+        <main className="h-full bg-gradient-to-r from-indigo-950 to-sky-950 overflow-auto">
             <div className="mx-auto max-w-screen-xl h-full w-full">
                 {children}
             </div>
